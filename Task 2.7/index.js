104966ab22e45f644f3a67dec47d1312d11cfaf0
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2023-10-11 13:54:18 +0400

    Add files via upload

---
+++ Task 2.7/index.js
@@ -0,0 +1,15 @@
+let eded=8;
+let bolen=1;
+let i=2;
+
+while(eded%i===0)
+{
+for(let i=2; i<eded; i++)
+
+    {
+        bolen=i; 
+    }
+}
+console.log(bolen);
+
+//7. Verilmiş ədədin ən böyük bölənini tapib, console çıxardın.
